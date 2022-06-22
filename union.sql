822b42f0b028e77cd470f4a572e2df99475ae374
nataliafelixx <88455034+nataliafelixx@users.noreply.github.com>
2022-06-22 00:07:36 -0300

    Create union.sql
    Exercício aula (UNION)

---
+++ union.sql
@@ -0,0 +1,6 @@
+-- (Exercício 1) União simples de duas tabelas
+-- Una a tabela sales.products com a tabela temp_tables.products_2
+
+select * from sales.products
+union all
+select * from temp_tables.products_2
